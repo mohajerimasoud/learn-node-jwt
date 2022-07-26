6c9232b486e65704c81d23baccea1a34bafc6467
Masoud Mohajeri <masoudmohajeri.wd@gmail.com>
2022-07-26 15:06:42 +0430

    add some utils - hash and jwt

---
+++ src/utils/jwt.js
@@ -0,0 +1,29 @@
+import jwt from "jsonwebtoken";
+
+export const generateAccessToken = (user) => {
+  return jwt.sign(
+    {
+      userId: user.id,
+    },
+    process.env.JWT_ACCESS_SECRET,
+    { expiresIn: "5m" }
+  );
+};
+
+export const generateRefreshToken = (user) => {
+  return jwt.sign(
+    {
+      userId: user.id,
+      jti,
+    },
+    process.env.JWT_ACCESS_SECRET,
+    { expiresIn: "8h" }
+  );
+};
+
+export const generateTokens = (user, jti) => {
+  const accessToken = generateAccessToken(user);
+  const refreshToken = generateRefreshToken(user);
+
+  return { accessToken, refreshToken };
+};
